05d2d402156e63780ad20a364435b1e8bcf7348f
Remora Development <info@nolasoft.com>
2018-04-18 04:39:42 +0000

    use correct config var

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo $0: starting bootstrap process
 
-curl -sGS --retry 3 --retry-delay 10 --data-urlencode id=$DB_BACKUPS_TESTING_ID --data-urlencode key=$1 https://app.remorabackup.com/get_util -o backup
+curl -sGS --retry 3 --retry-delay 10 --data-urlencode id=$REMORA_BACKUP_ID --data-urlencode key=$1 https://app.remorabackup.com/get_util -o backup
 
 chmod 700 ./backup
 
